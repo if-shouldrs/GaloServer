5d00c4bbc1249bd19d4c838dd28ad55e562fdf75
Wolf <71544385+if-shouldrs@users.noreply.github.com>
2024-03-23 15:59:50 +0000

    Defined 1st pass game logic

--- src/main/java/com/miniclip/galo/afonso/server/service/MatchServiceImpl.java
+++ src/main/java/com/miniclip/galo/afonso/server/service/MatchServiceImpl.java
@@ -4,6 +4,8 @@ import com.miniclip.galo.afonso.server.dto.MoveRequest;
 import com.miniclip.galo.afonso.server.exception.InvalidMoveException;
 import com.miniclip.galo.afonso.server.exception.MatchNotFoundException;
 import com.miniclip.galo.afonso.server.model.Match;
+import com.miniclip.galo.afonso.server.model.MatchStatus;
+import com.miniclip.galo.afonso.server.model.Player;
 import com.miniclip.galo.afonso.server.repository.MatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -47,15 +49,79 @@ public class MatchServiceImpl implements MatchService {
     }
 
     private boolean isValidMove(Match match, MoveRequest move) {
-        throw new UnsupportedOperationException("Not implemented");
+        if (!MatchStatus.RUNNING.equals(match.getStatus())) {
+            return false;
+        }
+
+        int row = move.getRow();
+        int col = move.getCol();
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+
+        // each row takes up 4 characters (3 for playable positions and 1 for the comma)
+        int position = (row * 4) + col;
+
+        // Check if the chosen position is empty
+        if (Character.isWhitespace(match.getBoardState().charAt(position))) {
+            return false;
+        }
+
+        // Ensure it is the correct player's turn
+        return match.getTurn().equals(move.getPlayer());
     }
 
     private void applyMove(Match match, MoveRequest move) {
-        throw new UnsupportedOperationException("Not implemented");
+        int position = (move.getRow() * 4) + move.getCol();
+
+        // Replace character at the move's position with the player's character
+        StringBuilder board = new StringBuilder(match.getBoardState());
+        board.setCharAt(position, move.getPlayer().getSymbol());
+
+        match.setBoardState(board.toString());
+
+        // Handle end of match conditions
+        processMatchStatus(match, move);
+    }
+
+    private void processMatchStatus(Match match, MoveRequest move) {
+        MatchStatus status = MatchStatus.RUNNING;
+
+        if (isWin(match, move)) {
+            status = move.getPlayer().equals(Player.PLAYER1) ? MatchStatus.PLAYER1WON : MatchStatus.PLAYER2WON;
+        } else if (isTie(match.getBoardState())) {
+            match.setStatus(MatchStatus.TIE);
+        } else {
+            match.setTurn(Player.getNext(move.getPlayer()));
+        }
+
+        match.setStatus(status);
+    }
+
+    private boolean isWin(Match match, MoveRequest move) {
+        char symbol = move.getPlayer().getSymbol();
+        String board = match.getBoardState();
+
+        // Check rows and columns
+        for (int i = 0; i < 3; i++) {
+            if ((board.charAt(i * 4) == symbol && board.charAt(i * 4 + 1) == symbol && board.charAt(i * 4 + 2) == symbol) || // Rows
+                    (board.charAt(i) == symbol && board.charAt(4 + i) == symbol && board.charAt(8 + i) == symbol)) { // Columns
+                return true;
+            }
+        }
+
+        // Check diagonals
+        return (board.charAt(0) == symbol && board.charAt(5) == symbol && board.charAt(10) == symbol) // Left-to-right
+                || (board.charAt(2) == symbol && board.charAt(5) == symbol && board.charAt(8) == symbol); // Right-to-left
+    }
+
+    private boolean isTie(String boardState) {
+        return !boardState.contains(" ");
     }
 
     @Override
     public List<Match> listAllMatches() {
         return new ArrayList<>(matchRepository.findAll());
     }
+
 }
