c8ddd63f3018cb4233fc052566bb4b5452c999a2
Wolf <71544385+if-shouldrs@users.noreply.github.com>
2024-03-23 20:16:12 +0000

    Removed unnecessary annotation data in test

--- src/test/java/com/miniclip/galo/afonso/server/service/GameServiceTest.java
+++ src/test/java/com/miniclip/galo/afonso/server/service/GameServiceTest.java
@@ -15,7 +15,7 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest(classes = GameServiceImpl.class)
+@SpringBootTest
 public class GameServiceTest {
 
     @Autowired
