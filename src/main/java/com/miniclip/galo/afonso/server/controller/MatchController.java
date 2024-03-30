c31f0be9c056ef4b0098c6f8ab371c5596d7d413
Wolf <71544385+if-shouldrs@users.noreply.github.com>
2024-03-30 15:22:34 +0000

    Fixed cross origin issues

--- src/main/java/com/miniclip/galo/afonso/server/controller/MatchController.java
+++ src/main/java/com/miniclip/galo/afonso/server/controller/MatchController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/matches")
 public class MatchController {
 
